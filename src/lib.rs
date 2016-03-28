295183d15e34579d95b198efa1698e0655133a15
Jussi Räsänen <jussi@skyred.fi>
2016-03-28 22:32:45 +0300

    [wip] Testing tweet return

--- src/lib.rs
+++ src/lib.rs
@@ -90,14 +90,17 @@ pub fn get_access_token(consumer: &Token,
 
 /// function to update the status
 /// This function takes as arguments the consumer key, the access key, and the status (obviously)
-pub fn update_status(consumer: &Token, access: &Token, status: &str) -> Result<String, Error> {
+pub fn update_status(consumer: &Token, access: &Token, status: &str) -> Result<Vec<Tweet>, Error> {
     let mut param = HashMap::new();
     let _ = param.insert("status".into(), status.into());
     let bytes = try!(oauth::post(api_twitter_soft::UPDATE_STATUS,
                              consumer,
                              Some(access),
                              Some(&param)));
-    Ok(String::from_utf8(bytes).unwrap())
+
+    let tweet_json = try!(String::from_utf8(bytes));
+    let t = try!(Tweet::parse_timeline(tweet_json));
+    Ok(t)
 }
 
 pub fn get_last_tweets(consumer: &Token, access: &Token) -> Result<Vec<Tweet>, Error> {
